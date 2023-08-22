de87d456ac178b233d81741bbe343af0f7123c27
Lee <leecook808@gmail.com>
2023-08-21 22:15:46 -0700

    Fix

--- src/main/java/lee/code/towns/commands/TabCompletion.java
+++ src/main/java/lee/code/towns/commands/TabCompletion.java
@@ -5,7 +5,9 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class TabCompletion implements TabCompleter {
 
@@ -17,6 +19,11 @@ public class TabCompletion implements TabCompleter {
 
     @Override
     public List<String> onTabComplete(@NonNull CommandSender sender, @NonNull Command command, @NonNull String alias, String[] args) {
-        return commandManager.getSubCommand(args[0].toLowerCase()).onTabComplete(sender, args);
+        final Optional<SubCommand> matchingSubCommand = commandManager.getSubCommands()
+                .stream()
+                .filter(subCommand -> args[0].equalsIgnoreCase(subCommand.getName()))
+                .findFirst();
+        return matchingSubCommand.map(subCommand -> subCommand.onTabComplete(sender, args))
+                .orElseGet(ArrayList::new);
     }
 }
