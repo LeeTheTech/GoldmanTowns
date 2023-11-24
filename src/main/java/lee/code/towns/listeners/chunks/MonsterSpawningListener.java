58ffcee2e582983106a10c2cf2ee25b512b1445d
Lee <leecook808@gmail.com>
2023-11-23 22:36:03 -0800

    spawn reason check

--- src/main/java/lee/code/towns/listeners/chunks/MonsterSpawningListener.java
+++ src/main/java/lee/code/towns/listeners/chunks/MonsterSpawningListener.java
@@ -9,9 +9,6 @@ import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.CreatureSpawnEvent;
-import org.bukkit.event.entity.EntitySpawnEvent;
-import org.bukkit.event.entity.SpawnerSpawnEvent;
-
 public class MonsterSpawningListener implements Listener {
   private final Towns towns;
 
@@ -21,38 +18,20 @@ public class MonsterSpawningListener implements Listener {
 
   @EventHandler
   public void onPreCreatureSpawn(PreCreatureSpawnEvent e) {
-    if (towns.getData().getMonsterTypes().contains(e.getType())) {
-      final MobSpawningEvent mobSpawningEvent = new MobSpawningEvent(e.getSpawnLocation());
-      Bukkit.getServer().getPluginManager().callEvent(mobSpawningEvent);
-      if (mobSpawningEvent.isCancelled()) e.setCancelled(true);
-    }
+    if (!towns.getData().getMonsterTypes().contains(e.getType())) return;
+    if (e.getReason().equals(CreatureSpawnEvent.SpawnReason.CUSTOM)) return;
+    final MobSpawningEvent mobSpawningEvent = new MobSpawningEvent(e.getSpawnLocation());
+    Bukkit.getServer().getPluginManager().callEvent(mobSpawningEvent);
+    if (mobSpawningEvent.isCancelled()) e.setCancelled(true);
   }
 
   @EventHandler
   public void onCreatureSpawn(CreatureSpawnEvent e) {
-    if (towns.getData().getMonsterTypes().contains(e.getEntity().getType())) {
-      final MobSpawningEvent mobSpawningEvent = new MobSpawningEvent(e.getLocation());
-      Bukkit.getServer().getPluginManager().callEvent(mobSpawningEvent);
-      if (mobSpawningEvent.isCancelled()) e.setCancelled(true);
-    }
-  }
-
-  @EventHandler
-  public void onEntitySpawn(EntitySpawnEvent e) {
-    if (towns.getData().getMonsterTypes().contains(e.getEntity().getType())) {
-      final MobSpawningEvent mobSpawningEvent = new MobSpawningEvent(e.getLocation());
-      Bukkit.getServer().getPluginManager().callEvent(mobSpawningEvent);
-      if (mobSpawningEvent.isCancelled()) e.setCancelled(true);
-    }
-  }
-
-  @EventHandler
-  public void onSpawnerSpawn(SpawnerSpawnEvent e) {
-    if (towns.getData().getMonsterTypes().contains(e.getEntity().getType())) {
-      final MobSpawningEvent mobSpawningEvent = new MobSpawningEvent(e.getLocation());
-      Bukkit.getServer().getPluginManager().callEvent(mobSpawningEvent);
-      if (mobSpawningEvent.isCancelled()) e.setCancelled(true);
-    }
+    if (!towns.getData().getMonsterTypes().contains(e.getEntity().getType())) return;
+    if (e.getSpawnReason().equals(CreatureSpawnEvent.SpawnReason.CUSTOM)) return;
+    final MobSpawningEvent mobSpawningEvent = new MobSpawningEvent(e.getLocation());
+    Bukkit.getServer().getPluginManager().callEvent(mobSpawningEvent);
+    if (mobSpawningEvent.isCancelled()) e.setCancelled(true);
   }
 
   @EventHandler
