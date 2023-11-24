8450985534dc9c39998402abc9e5061efa9fb4fa
Lee <leecook808@gmail.com>
2023-11-24 00:38:09 -0800

    dmg flag fix and lore fixes

--- src/main/java/lee/code/towns/listeners/chunks/DamageListener.java
+++ src/main/java/lee/code/towns/listeners/chunks/DamageListener.java
@@ -11,9 +11,6 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
-import org.bukkit.event.hanging.HangingBreakByEntityEvent;
-import org.bukkit.event.hanging.HangingBreakEvent;
-import org.bukkit.event.vehicle.VehicleDestroyEvent;
 
 public class DamageListener implements Listener {
   private final Towns towns;
@@ -23,8 +20,8 @@ public class DamageListener implements Listener {
   }
 
   @EventHandler
-  public void onEntityDamage(EntityDamageEvent e) {
-    if (towns.getData().getMonsterTypes().contains(e.getEntity().getType())) return;
+  public void onPlayerDamage(EntityDamageEvent e) {
+    if (!(e.getEntity() instanceof Player)) return;
     if (e.getCause().equals(EntityDamageEvent.DamageCause.ENTITY_ATTACK)) return;
     final DamageEvent damageEvent = new DamageEvent(e.getEntity().getLocation());
     Bukkit.getServer().getPluginManager().callEvent(damageEvent);
@@ -32,35 +29,13 @@ public class DamageListener implements Listener {
   }
 
   @EventHandler
-  public void onEntityDamageByEntityListener(EntityDamageByEntityEvent e) {
+  public void onPlayerDamageByEntity(EntityDamageByEntityEvent e) {
     if (e.getDamager() instanceof Player) return;
     if (e.getDamager() instanceof Projectile projectile) {
       if (projectile.getShooter() instanceof Player) return;
     }
+    if (towns.getData().getMonsterTypes().contains(e.getEntity().getType())) return;
     final DamageEvent damageEvent = new DamageEvent(e.getEntity().getLocation());
-    Bukkit.getServer().getPluginManager().callEvent(damageEvent);
-    if (damageEvent.isCancelled()) e.setCancelled(true);
-  }
-
-  @EventHandler
-  public void onHangingBreakListener(HangingBreakEvent e) {
-    final DamageEvent damageEvent = new DamageEvent(e.getEntity().getLocation());
-    Bukkit.getServer().getPluginManager().callEvent(damageEvent);
-    if (damageEvent.isCancelled()) e.setCancelled(true);
-  }
-
-  @EventHandler
-  public void onHangingBreakByEntityListener(HangingBreakByEntityEvent e) {
-    if (e.getRemover() instanceof Player) return;
-    final DamageEvent damageEvent = new DamageEvent(e.getEntity().getLocation());
-    Bukkit.getServer().getPluginManager().callEvent(damageEvent);
-    if (damageEvent.isCancelled()) e.setCancelled(true);
-  }
-
-  @EventHandler
-  public void onVehicleDestroyListener(VehicleDestroyEvent e) {
-    if (e.getAttacker() instanceof Player) return;
-    final DamageEvent damageEvent = new DamageEvent(e.getVehicle().getLocation());
     Bukkit.getServer().getPluginManager().callEvent(damageEvent);
     if (damageEvent.isCancelled()) e.setCancelled(true);
   }
